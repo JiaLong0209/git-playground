d92c51464a0a8f8bd12e0f815efedcc9fd2adceb
JiaLong0209 <smt02091395@gmail.com>
2023-11-25 09:32:16 +0800

    231125 Add more lua file

---
+++ lua/metaTable2.lua
@@ -0,0 +1,25 @@
+-- Creating a table with a custom __tostring metamethod
+local people = {name = 'jialong', color = 'red'}
+local person = {name = "John", age = 30}
+
+setmetatable(person, {
+    __tostring = function(self)
+        return "Person: " .. self.name .. ", Age: " .. self.age
+    end,
+    __index = people
+})
+
+-- Using tostring() to convert the table to a string
+print(tostring(person))  -- Outputs: Person: John, Age: 30
+
+print(person.name)
+print(person.color)
+
+print(
+  setmetatable(person, {
+      __tostring = function(self)
+          return "Person: " .. self.name .. ", Age: " .. self.age
+      end,
+      __index = people
+  }).name
+)
