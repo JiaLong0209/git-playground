775691f8b21d1c5e93661627f7bad3709bab6868
JiaLong0209 <smt02091395@gmail.com>
2023-11-26 00:49:39 +0800

    231126 Refactor code in function.lua

--- lua/function.lua
+++ lua/function.lua
@@ -1,11 +1,10 @@
 local function printByTimes(str)
-  return function (x) for i = 1, x or 1 do print(i .. ' hello, ' .. str) end end
+  return function(x) for i = 1, x or 1 do print(i .. ' hello, ' .. str) end end
 end
 
+local terms = { 'world', 'jialong', 'allen' }
+local times = { 2, 2, 3 }
 
-
-printByTimes('world')(3)
-printByTimes('jialong')(2)
-
-
-
+for index, value in ipairs(terms) do
+  printByTimes(value)(times[index])
+end
