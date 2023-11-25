8b96614465fb720ec9b69be9905e63b9b4d2082a
JiaLong0209 <smt02091395@gmail.com>
2023-11-26 01:25:53 +0800

    231126 Rename file functions.lua

---
+++ lua/functions.lua
@@ -0,0 +1,20 @@
+module = {}
+
+function module.printByTimes(str)
+  return function(x) for i = 1, x or 1 do print(i .. ' hello, ' .. str) end end
+end
+
+function module.hello(fun)
+  return function(str) fun('hello, ' .. str) end
+end
+
+-- local terms = { 'world', 'jialong', 'allen' }
+-- local times = { 2, 2, 3 }
+
+-- for index, value in ipairs(terms) do
+--   printByTimes(value)(times[index])
+-- end
+
+-- return {printTimes = printByTimes, hello = hello}
+
+return module
