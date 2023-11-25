600910fb64fe25ee541455561f53a8880a87817b
JiaLong0209 <smt02091395@gmail.com>
2023-11-26 00:36:32 +0800

    231126 Rename a function name

--- lua/function.lua
+++ lua/function.lua
@@ -1,8 +1,11 @@
-local function hello(str)
+local function printByTimes(str)
   return function (x) for i = 1, x or 1 do print(i .. ' hello, ' .. str) end end
 end
 
 
 
-hello('world')(3)
-hello('jialong')(2)
+printByTimes('world')(3)
+printByTimes('jialong')(2)
+
+
+
